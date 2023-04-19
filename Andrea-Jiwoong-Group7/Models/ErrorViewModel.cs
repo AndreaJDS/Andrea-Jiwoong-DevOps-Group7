e33896080efbc580fa9ef253a121c3b39298e6fa
AndreaJDS <andrea_jolene@outlook.com>
2023-04-19 12:08:46 -0400

    initial commit

---
+++ Andrea-Jiwoong-Group7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Andrea_Jiwoong_Group7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
